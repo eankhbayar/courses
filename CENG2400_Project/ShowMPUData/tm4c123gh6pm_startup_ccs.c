7546db4ece0c1c69955fc717703a1faa81cc7e1f
Ankhbayar Enkhtaivan <91533032+eankhbayar@users.noreply.github.com>
2023-11-24 12:37:43 +0800

    Update tm4c123gh6pm_startup_ccs.c

--- CENG2400_Project/ShowMPUData/tm4c123gh6pm_startup_ccs.c
+++ CENG2400_Project/ShowMPUData/tm4c123gh6pm_startup_ccs.c
@@ -41,6 +41,7 @@ static void IntDefaultHandler(void);
 //
 //*****************************************************************************
 extern void _c_int00(void);
+extern void UARTIntHandler(void);
 
 //*****************************************************************************
 //
@@ -88,7 +89,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port B
     IntDefaultHandler,                      // GPIO Port C
     IntDefaultHandler,                      // GPIO Port D
-    IntDefaultHandler,                      // GPIO Port E
+    UARTIntHandler,                      // GPIO Port E
     IntDefaultHandler,                      // UART0 Rx and Tx
     IntDefaultHandler,                      // UART1 Rx and Tx
     IntDefaultHandler,                      // SSI0 Rx and Tx
