a4489829e10882ff6546390ea4eaf7e7d91de45d
Ankhbayar Enkhtaivan <91533032+eankhbayar@users.noreply.github.com>
2023-11-24 11:11:06 +0800

    Update main.c

--- CENG2400_Project/ShowMPUData/main.c
+++ CENG2400_Project/ShowMPUData/main.c
@@ -113,7 +113,9 @@ void MPU6050Callback(void *pvCallbackData, uint_fast8_t ui8Status){
     // See if an error occurred.
     if (ui8Status != I2CM_STATUS_SUCCESS){
         // An error occurred, so handle it here if required.
+        UARTprintf("ERROR", UART0_BASE);
     }
+    UARTprintf("NOTERROR", UART0_BASE);
     // Indicate that the MPU6050 transaction has completed.
     g_bMPU6050Done = true;
 }
@@ -192,6 +194,21 @@ void MPU6050Example(int *pitch, int *roll, int *yaw)
 int main(){
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
 
+    // UART0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    // set GPIO A0 and A1 as UART pins.
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    // set GPIO A0 and A1 as UART pins.
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    // Initialize the UART for console I/O.
+    UARTStdioConfig(0, 115200, SysCtlClockGet());
+    UARTCharPut(UART0_BASE, 'v');
+    UARTprintf("Started:", UART0_BASE);
+
     // initialize I2C, you may do not care this part
     InitI2C0();
 
@@ -201,57 +218,20 @@ int main(){
 
     int X = 0, Y = 0, Z = 0;
 
-    // initialize UART
-    // UART0 is used to show data on the screen
-    // UART1 is used to send data to the servo
-
-    // UART0
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
-    // set GPIO A0 and A1 as UART pins.
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
-
-    // set GPIO A0 and A1 as UART pins.
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    // Initialize the UART for console I/O.
-    UARTStdioConfig(0, 115200, SysCtlClockGet());
-
-    // UART1
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-
-    // set GPIO B0 and B1 as UART pins.
-    GPIOPinConfigure(GPIO_PB0_U1RX);
-    GPIOPinConfigure(GPIO_PB1_U1TX);
-
-    // set GPIO B0 and B1 as UART pins.
-    GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    // Initialize the UART for console I/O.
-    UARTStdioConfig(1, 115200, SysCtlClockGet());
-
-    // initialize MPU6050
-    // you may do not care this part
-    g_bMPU6050Done = false;
-    MPU6050Init(&g_sI2CMSimpleInst, MPU6050Callback, &g_sMPU6050Inst);
-    while (!g_bMPU6050Done){}
-    g_bMPU6050Done = false;
-
     while(1){
         // get raw data from MPU6050
         MPU6050Example(&X, &Y, &Z);
         // scale to a proper Master rotation
         //pitch, roll, yaw
         // x,     y,    z
-
-        if(x > 110) x = 110;
-        if(x < 20) x = 20;
-
-        if(y > 160) y = 160;
-        if(y < 20) y = 20;
+        if(g_bMPU6050Done == true){
+            UARTprintf("MPUDONE", UART0_BASE);
+        }
+//        if(X > 110) X = 110;
+//        if(X < 20) X = 20;
+//
+//        if(Y > 160) Y = 160;
+//        if(Y < 20) Y = 20;
 
         // send data via UART
         // example: R aaa bbb ccc x
@@ -259,8 +239,8 @@ int main(){
         // x is a flag to indicate the end of a package
         // you may design your own package format
         //UARTprintf("R %d %d %d x\n", X, Y, Z);
-        UARTprintf("R %d %d %d x\n", x, y, z);
-        
+        UARTprintf("R %d %d %d x\n", X, Y, Z, UART0_BASE);
+
 
         // NOTE:
         // the raw data of these three axes are all from -180 to 180.
@@ -279,6 +259,5 @@ int main(){
 
     return(0);
 }
-
-
+void UARTIntHandler(void){}
 
